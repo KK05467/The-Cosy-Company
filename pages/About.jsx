5a7ff05a7dfff655d4afdd2822ebc51ac9920242
Keertan <keertan2005@gmail.com>
2026-06-05 22:40:16 +0530

    about added

---
+++ pages/About.jsx
@@ -0,0 +1,15 @@
+function About({ darkMode }) {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        padding: "120px 80px",
+        color: darkMode ? "white" : "#0f172a",
+      }}
+    >
+      <h1>About Cosy</h1>
+    </div>
+  )
+}
+
+export default About
