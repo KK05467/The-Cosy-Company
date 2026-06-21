7210692f6d552bfff811f96696494c6501d5be88
Keertan <keertan2005@gmail.com>
2026-06-21 22:44:58 +0530

    update

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -11,6 +11,12 @@ import {
 
 import { protectRoute } from "../middleware/authMiddleware.js";
 
+import upload from "../middleware/upload.js";
+import {
+  uploadProfilePicture
+}
+from "../controllers/authController.js";
+
 const router = express.Router();
 
 router.post("/signup", signup);
@@ -22,5 +28,11 @@ router.post("/reset-password", resetPassword);
 router.get("/profile", protectRoute, getProfile);
 
 router.put("/profile", protectRoute, updateProfile);
+router.put(
+  "/upload-profile-picture",
+  protectRoute,
+  upload.single("image"),
+  uploadProfilePicture
+);
 
 export default router;
--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { protectRoute } from "../middleware/authMiddleware.js";
 import { createBooking, getMyBookings,getBookingById, } from "../controllers/bookingController.js";
+import { cancelBooking } from "../controllers/bookingController.js";
 
 const router = express.Router();
 
 router.post("/", protectRoute, createBooking);
 router.get("/my", protectRoute, getMyBookings);
 router.get("/:id", protectRoute, getBookingById);
+router.patch("/cancel/:id", protectRoute, cancelBooking);
 
 export default router;
