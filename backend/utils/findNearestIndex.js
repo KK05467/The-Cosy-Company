4615d26c6a08d98750ee001d85dca0d687178df5
Keertan <keertan2005@gmail.com>
2026-06-21 22:45:58 +0530

    update

--- backend/utils/findNearestIndex.js
+++ backend/utils/findNearestIndex.js
@@ -1,63 +1,31 @@
-function distanceInMeters(p1, p2) {
+/**
+ * findNearestIndex.js
+ * Finds the index of the closest point in routeCoordinates to the given point.
+ *
+ * point            : { lat, lng }
+ * routeCoordinates : [{ lat, lng }, ...]
+ *
+ * Returns: Number (index), or -1 if coordinates are empty.
+ */
 
-  const R = 6371000;
+import { distanceInMeters } from "./haversine.js";
 
-  const lat1 = p1[1] * Math.PI / 180;
-  const lat2 = p2[1] * Math.PI / 180;
-
-  const dLat =
-    (p2[1] - p1[1]) * Math.PI / 180;
-
-  const dLng =
-    (p2[0] - p1[0]) * Math.PI / 180;
-
-  const a =
-    Math.sin(dLat / 2) *
-    Math.sin(dLat / 2) +
-    Math.cos(lat1) *
-    Math.cos(lat2) *
-    Math.sin(dLng / 2) *
-    Math.sin(dLng / 2);
-
-  const c =
-    2 *
-    Math.atan2(
-      Math.sqrt(a),
-      Math.sqrt(1 - a)
-    );
-
-  return R * c;
-}
-
-export function findNearestIndex(
-  point,
-  routeCoordinates
-) {
-
-  let index = 0;
-  let minDistance = Infinity;
-
-  for (
-    let i = 0;
-    i < routeCoordinates.length;
-    i++
-  ) {
-
-    const distance =
-      distanceInMeters(
-        point,
-        routeCoordinates[i]
-      );
-
-    if (distance < minDistance) {
-
-      minDistance = distance;
-      index = i;
-
-    }
-
+export function findNearestIndex(point, routeCoordinates) {
+  if (!point || !routeCoordinates || routeCoordinates.length === 0) {
+    return -1;
   }
 
-  return index;
+  let nearestIndex = -1;
+  let minDistance = Infinity;
 
+  for (let i = 0; i < routeCoordinates.length; i++) {
+    const distance = distanceInMeters(point, routeCoordinates[i]);
+
+    if (distance < minDistance) {
+      minDistance = distance;
+      nearestIndex = i;
+    }
+  }
+
+  return nearestIndex;
 }
