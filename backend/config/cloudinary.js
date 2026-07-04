dc0cc4e18a7d4c4fc450ef24cedd8bb48705d3e2
KEERTAN KUMAR <keertan2005@gmail.com>
2026-07-04 17:38:44 +0530

    Fix syntax error in cloudinary.js configuration

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -4,6 +4,6 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
-});
+})
 
 export default cloudinary;
