65c7278b24624e5fbf3731cb13ae1a1ee36bf42a
KEERTAN KUMAR <keertan2005@gmail.com>
2026-07-04 17:39:50 +0530

    Update MongoDB connection success message

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose"
 
-
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI)
 
-    console.log("MongoDB Connected")
+    console.log("MongoDB Connected Successfully")
   } catch (error) {
     console.error(error)
     process.exit(1)
