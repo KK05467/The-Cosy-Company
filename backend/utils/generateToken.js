61135e8a9bbbb2962999171d2a822110fbfea09a
KEERTAN KUMAR <keertan2005@gmail.com>
2026-07-14 18:16:34 +0530

    Fix token generation syntax error

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken")
 const generateToken = (id) => {
   return jwt.sign(
     { id },
-    process.env.JWT_SECRET,
+    process.env.JWT_SECRET
     {
       expiresIn: "30d",
     }
